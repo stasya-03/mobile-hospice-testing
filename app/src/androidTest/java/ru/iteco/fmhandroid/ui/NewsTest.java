184da356c0e5943734c25470bbdec7de3a8af6ef
Anastasia <nastya.il03@mail.ru>
2026-06-09 15:19:23 +0400

    Add NewsTest

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/NewsTest.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/NewsTest.java
@@ -7,6 +7,8 @@ import androidx.test.filters.LargeTest;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Before;
+import org.junit.After;
 
 import ru.iteco.fmhandroid.page.AuthorizationPage;
 import ru.iteco.fmhandroid.page.MainPage;
@@ -24,17 +26,46 @@ public class NewsTest {
     public ActivityScenarioRule<AppActivity> activityScenarioRule =
             new ActivityScenarioRule<>(AppActivity.class);
 
-    @Test
-    public void shouldCreateNews() {
-        String title = "Back massage";
-        String description = "Nothing special";
+    @Before
+    public void setUp() {
+        try {
+            mainPage.logOut();
+            authorizationPage.waitAuthorizationScreen();
+        } catch (Exception ignored) {
+        }
 
         authorizationPage.login("login2", "password2");
         mainPage.checkMainPageIsDisplayed();
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            mainPage.logOut();
+            authorizationPage.waitAuthorizationScreen();
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    public void shouldCreateNews() {
+        String title = "Back massage " + System.currentTimeMillis();
+        String description = "Nothing special";
         mainPage.openMainMenu();
         mainPage.clickNewsInMenu();
         newsPage.createNews(title, description);
         newsPage.checkNewsIsDisplayed(title);
+    }
+
+    @Test
+    public void shouldCancelNewsCreation() {
+        mainPage.openMainMenu();
+        mainPage.clickNewsInMenu();
+        newsPage.openControlPanel();
+        newsPage.clickAddNews();
+        newsPage.clickCancel();
+        newsPage.confirmCancelNews();
+        newsPage.checkControlPanelIsDisplayed();
     }
 
 
